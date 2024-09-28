1a64a9817e556dd4863edd054a5165cddb53a979
Kuldeep <kuldeeplovanshi777@gmail.com>
2024-09-28 18:40:09 +0530

    ruby concept

---
+++ ruby.rb
@@ -0,0 +1,45 @@
+class Employee
+  @@all_employee =[]
+
+  attr_accessor :name, :department
+
+  def initialize(name, department)
+      @name = name
+      @department = department
+      pp @@all_employee <<self
+  end
+
+  def self.all_employees
+     pp @@all_employee
+  end
+end
+
+employee1 = Employee.new('joh','IT')
+# self.all_employees is class method
+Employee.all_employees
+
+class Demo
+  private
+  def exp
+    pp "this is private method"
+  end
+end
+
+obj = Demo.new
+obj.send:exp
+obj.instance_eval{exp}
+
+
+
+# how to create alias of a method
+
+class Example
+  def car 
+    pp "test alias method"
+  end
+
+  alias test car
+end
+
+example = Example.new
+example.test
