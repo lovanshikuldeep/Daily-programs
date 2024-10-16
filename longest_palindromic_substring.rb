ae2b63ea87cb9abab148a37a56fd1bde38695e8a
Kuldeep <kuldeeplovanshi777@gmail.com>
2024-10-16 17:29:24 +0530

    correct indent

--- longest_palindromic_substring.rb
+++ longest_palindromic_substring.rb
@@ -1,28 +1,28 @@
-5. Longest Palindromic Substring
-Solved
-Medium
-Topics
-Companies
-Hint
-Given a string s, return the longest
-palindromic
+# 5. Longest Palindromic Substring
+# Solved
+# Medium
+# Topics
+# Companies
+# Hint
+# Given a string s, return the longest
+# palindromic
 
-substring
- in s.
+# substring
+#  in s.
 
 
 
-Example 1:
+# Example 1:
 
-Input: s = "babad"
-Output: "bab"
-Explanation: "aba" is also a valid answer.
-Example 2:
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+# Example 2:
 
-Input: s = "cbbd"
-Output: "bb"
+# Input: s = "cbbd"
+# Output: "bb"
 
-solution
+# solution
 
 # @param {String} s
 # @return {String}
